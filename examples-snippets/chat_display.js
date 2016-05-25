4bdf5e114497bbbd3510f36d788db4647b5e07fc
Njibhu <manu@njibhu.eu>
2016-05-25 19:14:08 +0200

    Fixes + some changes !
    Fixing the event READY.
    Messages, Channels, Servers, Conversations are directly accessible from
    the client now, using client.channels[channelID] or
    client.servers[serverID]... You can also iterate over them in their
    parent class, for example: parent of server is client, so the
    Client.serverList is an array ! same for channelList inside the Server
    class and the messageList inside the Conversation class ! Everything is
    easier :)

--- examples-snippets/chat_display.js
+++ examples-snippets/chat_display.js
@@ -3,27 +3,13 @@
 var cursejs = require('../lib/index.js');
 var client = new cursejs.Client;
 
-var serverChannels = {};
-
-//we're just registering all the conversations ID into on object here
-//Because we can't have directly channel names from conversations
-//So we register all channels into one variable
-//And see if a conversation correspond to it inside the message_received handler
-client.on('ready', function(){
-    Object.keys(client.serverList).forEach(function(skey){
-        var server = client.serverList[skey];
-        Object.keys(server.channelList).forEach(function(cKey){
-            var channel = server.channelList[cKey];
-            serverChannels[channel.ID] = channel;
-        });
-    });
-});
-
 //Show the current received messages !
 client.on('message_received', function(message){
+    //Will display the conversation ID if not a message in a channel
     var prefix = message.conversation.ID;
-    if(serverChannels[message.conversation.ID] != undefined){
-        prefix = serverChannels[message.conversation.ID].name;
+    //Look for a channel corresponding to the conversation ID
+    if(client.channels[message.conversation.ID] != undefined){
+        prefix = client.channels[message.conversation.ID].name;
     }
     console.log(prefix, "[", message.senderName, "]", ": ", message.content);
 });
