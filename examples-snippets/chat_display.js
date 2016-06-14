c23a2fa7c371cd5b8e023f138a7a876603fca72b
Njibhu <manu@njibhu.eu>
2016-06-14 23:42:13 +0200

    Transform some objects to map, and pre work for better history message handling

--- examples-snippets/chat_display.js
+++ examples-snippets/chat_display.js
@@ -8,8 +8,8 @@ client.on('message_received', function(message){
     //Will display the conversation ID if not a message in a channel
     var prefix = message.conversation.ID;
     //Look for a channel corresponding to the conversation ID
-    if(client.channels[message.conversation.ID] != undefined){
-        prefix = client.channels[message.conversation.ID].name;
+    if(client.channels.has(message.conversation.ID)){
+        prefix = client.channels.get(message.conversation.ID).name;
     }
     console.log(prefix, "[", message.senderName, "]", ": ", message.content);
 });
